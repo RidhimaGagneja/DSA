cd8c3746871ebc092bee0ec3d31c71b1635d846f
Ridhima Gagneja <ridhima.gagneja06@gmail.com>
2026-08-15 20:03:21 +0530

    added practise questions

---
+++ patterns.cpp/contNumtriangle.cpp
@@ -0,0 +1,34 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter n:";
+//     cin>>n;
+
+//     int num=1;
+
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<num;
+//             num=num+1;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+
+    int num=1;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
